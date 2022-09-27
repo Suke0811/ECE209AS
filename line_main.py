fe87b45afa78261352f0ebdf54cbd326eaf9a8e6
Suke0811 <49264928+Suke0811@users.noreply.github.com>
2022-09-27 10:58:00 -0700

    Fix in line import

--- line_main.py
+++ line_main.py
@@ -3,8 +3,7 @@ from rems.Config import SimConfig
 from rems.inputs import KeyboardInput
 from rems.outputs import FileOutput
 from rems.utils import time_str
-from line import LineAnimation, LineWorldSystem, LineWorldDef
-
+from line import LineAnimation, LineWorldSystem
 
 o = Operator(debug_mode=True)
 i = KeyboardInput(dict(v=0, y=0), enable_keys=['up', 'down'])
